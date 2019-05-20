7cb2214c9bb5b8073933ba56deff85ee8f706eb3
Gabriel Ernesto Manuel Martinez Escobar <gernestome@hotmail.com>
2019-05-19 19:45:05 -0500

    lab09 TouchableOpacity

--- src/components/BookRow.js
+++ src/components/BookRow.js
@@ -1,28 +1,43 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {
     View,
     Text,
-    StyleSheet
+    StyleSheet,
+    TouchableOpacity
 } from 'react-native';
 
 const BookRow = ({book, index}) => {
+    const [showInfo, setShowInfo] = useState(false);
     return (
-        <View
-            key={index}
-            style={[
-              styles.row,
-              { backgroundColor: index % 2 === 0 ? 'white' : '#F3F3F7' }
-            ]}>
-            <View style={styles.edges}>
-                <Text>{index + 1 } </Text>
+        <View>
+            <View
+                key={index}
+                style={[
+                styles.row,
+                { backgroundColor: index % 2 === 0 ? 'white' : '#F3F3F7' }
+                ]}>
+                <View style={styles.edges}>
+                    <Text>{index + 1 } </Text>
+                </View>
+                <View style={styles.titleBook}>
+                    <Text>{book.title}</Text>
+                    <Text style={styles.author}>{`de ${book.author}`}</Text>
+                </View>
+                <View style={styles.edges}>
+                <TouchableOpacity
+                    onPress={() => {
+                        setShowInfo(!showInfo);
+                    }}
+                    style={styles.button}
+                >
+                    <Text>Info</Text>
+                </TouchableOpacity>
+                </View>
             </View>
-            <View style={styles.titleBook}>
-                <Text>{book.title}</Text>
-                <Text style={styles.author}>{`de ${book.author}`}</Text>
-            </View>
-            <View style={styles.edges}>
-                <Text>Info</Text>
-            </View>
+            {showInfo &&
+                <View>
+                    <Text>Book Info</Text>
+                </View>}
         </View>
     )
 };
@@ -36,11 +51,19 @@ const styles = StyleSheet.create({
     'edges': {
       flex:1,
       alignItems: 'center',
-      justifyContent: 'center'
+      justifyContent: 'center',
+      minWidth: 50
     },
     'titleBook': {
         flex: 8,
         flexDirection: 'column'
     },
-    'author': { color: 'grey'}
+    'author': { color: 'grey'},
+    button: {
+        borderWidth: 1,
+        borderColor: '#000000',
+        borderRadius: 10,
+        paddingHorizontal: 10,
+        paddingVertical: 2
+    }
   });
